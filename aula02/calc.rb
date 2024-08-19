3e9e00e735d5f610ed8bfd01b295a2c8d2e4a8eb
Sergio Oseko <osk.sergio@gmail.com>
2024-08-19 17:42:20 -0300

    feat: aula 2 / leitura e manipulação de variáveis

---
+++ aula02/calc.rb
@@ -0,0 +1,6 @@
+print 'Digite o primeiro número inteiro: '
+number1 = gets.chomp.to_i
+print 'Digite o segundo número inteiro : '
+number2 = gets.chomp.to_i
+sum_1_an_2 = number1 + number2
+puts "O resultado da soma entre os dois número é: #{sum_1_an_2}"
---
+++ aula02/hello.rb
@@ -0,0 +1,9 @@
+print 'Digite seu nome: '
+
+name = gets.chomp
+
+print 'Digite o seu sobrenome: '
+
+last_name = gets.chomp
+
+puts "Hello #{name} #{last_name}!"
